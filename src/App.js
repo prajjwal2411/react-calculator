59df899537b858f13d6af6dc6dc0ec4a72af1c4f
PrajjwalSingh24 <prajjwal.singh@delhivery.com>
2023-12-24 22:40:12 +0530

    calculator-logic-done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [ numericalString, setNumericalString ] = useState('');
+
+  function handleClick(event) {
+    let value = event.target.value;
+    switch(value){
+      case '=':
+        let ans;
+        try{
+          ans = eval(numericalString)
+        } catch {
+          ans = 'Math Error';
+          setNumericalString('Math Error');
+        } finally {
+          setNumericalString(ans.toString());
+        }
+        break;
+      case 'delete':
+        setNumericalString(numericalString.substring(0, numericalString.length-1));
+        break;
+      case 'clear':
+        setNumericalString('');
+        break;
+      default:
+        setNumericalString(numericalString+value);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Calculator App</h1>
+      {/** Calculation Screen */}
+      <input type="text" readOnly value={numericalString}/>
+      
+      {/** Number Pad */}
+      <div>
+        <button onClick={event => handleClick(event)} value={9}>9</button>
+        <button onClick={event => handleClick(event)} value={8}>8</button>
+        <button onClick={event => handleClick(event)} value={7}>7</button>
+      </div>
+      <div>
+        <button onClick={event => handleClick(event)} value={6}>6</button>
+        <button onClick={event => handleClick(event)} value={5}>5</button>
+        <button onClick={event => handleClick(event)} value={4}>4</button>
+      </div>
+      <div>
+        <button onClick={event => handleClick(event)} value={3}>3</button>
+        <button onClick={event => handleClick(event)} value={2}>2</button>
+        <button onClick={event => handleClick(event)} value={1}>1</button>
+      </div>
+      <div>
+        <button onClick={event => handleClick(event)} value={'.'}>.</button>
+        <button onClick={event => handleClick(event)} value={0}>0</button>
+      </div>
+      <div>
+        <button onClick={event => handleClick(event)} value={'+'}>+</button>
+        <button onClick={event => handleClick(event)} value={'-'}>-</button>
+        <button onClick={event => handleClick(event)} value={'*'}>*</button>
+        <button onClick={event => handleClick(event)} value={'/'}>/</button>
+      </div>
+      <div>
+        <button onClick={event => handleClick(event)} value={'='}>=</button>
+        <button onClick={event => handleClick(event)} value={'delete'}>Delete</button>
+        <button onClick={event => handleClick(event)} value={'clear'}>Clear</button>
+      </div>
+    </>
   );
 }
 
